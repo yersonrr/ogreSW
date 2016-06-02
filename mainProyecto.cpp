70c908308c9e304fa1274c76ffde951eb34dab5f
Yerson Roa <yerson132@gmail.com>
2016-06-02 01:00:03 -0430

    First Commit

---
+++ mainProyecto.cpp
@@ -0,0 +1,57 @@
+#include "Ogre\ExampleApplication.h"
+
+class Example1 : public ExampleApplication
+{
+
+public:
+
+	void createCamera() {
+
+		mCamera = mSceneMgr->createCamera("MyCamera1");
+		mCamera->setPosition(0,10,50);
+		mCamera->lookAt(0,0,-50);
+		mCamera->setNearClipDistance(5);
+
+	}
+
+	void createScene()
+	{
+
+		mSceneMgr->setAmbientLight(Ogre::ColourValue(1.0, 1.0, 1.0));
+		mSceneMgr->setShadowTechnique(Ogre::SHADOWTYPE_STENCIL_ADDITIVE);
+		
+
+		Ogre::Entity* ent01 = mSceneMgr->createEntity("MyEntity1","ejes01.mesh");
+		Ogre::SceneNode* node01 = mSceneMgr->createSceneNode("Node01");
+		mSceneMgr->getRootSceneNode()->addChild(node01);
+		node01->attachObject(ent01);
+		
+
+		Ogre::Entity* entEscena01 = mSceneMgr->createEntity("ogre_base01.mesh");
+		mSceneMgr->getRootSceneNode()->attachObject(entEscena01);
+
+		Ogre::Entity* entEscena02 = mSceneMgr->createEntity("ogre_fondo01.mesh");
+		mSceneMgr->getRootSceneNode()->attachObject(entEscena02);
+
+		Ogre::Entity* entEscena03 = mSceneMgr->createEntity("ogre_pared01.mesh");
+		mSceneMgr->getRootSceneNode()->attachObject(entEscena03);
+
+		Ogre::Entity* entEscena04 = mSceneMgr->createEntity("ogre_pared02.mesh");
+		mSceneMgr->getRootSceneNode()->attachObject(entEscena04);
+
+		Ogre::Entity* entEscena05 = mSceneMgr->createEntity("ogre_pared03.mesh");
+		mSceneMgr->getRootSceneNode()->attachObject(entEscena05);
+
+		Ogre::Entity* entEscena06 = mSceneMgr->createEntity("ogre_torretas01.mesh");
+		mSceneMgr->getRootSceneNode()->attachObject(entEscena06);
+	}
+
+};
+
+
+int main (void)
+{
+	Example1 app;
+	app.go();
+	return 0;
+}
